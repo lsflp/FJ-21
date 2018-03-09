e3632ea1d81d3065abf32032387218b21ded6a83
Luís Felipe <lipeluiis@hotmail.com>
2018-03-09 19:24:41 -0300

    Fazendo até login e logout

--- workspace/fj21-tarefas/src/br/com/caelum/tarefas/controller/LoginController.java
+++ workspace/fj21-tarefas/src/br/com/caelum/tarefas/controller/LoginController.java
@@ -24,4 +24,10 @@ public class LoginController {
 		}
 		return "redirect:loginForm";
 	}
+	
+	@RequestMapping("logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:loginForm";
+	}
 }
